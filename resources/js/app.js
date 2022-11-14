8fed9801cb3a71a7f7ec7e5e5af72cb98d57ad38
theocop73 <theo.colombel@le-campus-numerique.fr>
2022-11-14 14:41:46 +0100

    setup project

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,12 @@
 require('./bootstrap');
 
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 import App from './App.vue'
 
+const pinia = createPinia()
+
 createApp(App)
+    .use(pinia)
     .mount('#app')
+    
