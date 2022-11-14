9348c494d19ea38b9939d809cd8fda5d6fcae7b8
theocop73 <theo.colombel@le-campus-numerique.fr>
2022-11-14 09:57:11 +0100

    ajout tailwind

---
+++ resources/js/App.vue
@@ -0,0 +1,36 @@
+
+<template>
+
+    <div>
+
+        <h1 class="text-3xl font-bold underline">
+    Hello world FROM VUE!
+  </h1>
+
+    </div>
+
+
+</template>
+
+<script>
+
+export default{
+    name: "App.vue",
+    mounted(){
+        console.log("HAAAAAAAAAAAAAAAAAAAAAAa")
+    },
+    beforeCreate(){
+        console.log("BEFORE CREATION")
+    }
+
+    
+}
+
+</script>
+
+
+<style scoped>
+    h1{
+        color:red
+    }
+</style>
