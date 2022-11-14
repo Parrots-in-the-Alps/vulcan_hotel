bd8b9cc8e8ea56e6849928be9f8371018fe19931
theocop73 <theo.colombel@le-campus-numerique.fr>
2022-11-14 10:55:02 +0100

    add .files

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hew/{test}', function ($test = null) {
+    return $test;
+});
