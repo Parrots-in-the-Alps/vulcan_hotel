ab642032aa8264573e4d075fcbaba5859568e616
PierreOudot <114479562+PierreOudot@users.noreply.github.com>
2022-11-17 16:48:02 +0100

    api video done

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -2,9 +2,46 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Video;
 use Illuminate\Http\Request;
 
 class VideoController extends Controller
 {
-    //
+    public function showVideos()
+    {
+        return response()->json([Video::all(), 'description' => 'OK'], 200);
+    }
+
+    public function showVideo($id)
+    {
+        return response()->json([Video::find($id), 'description' => 'OK'], 200);
+    }
+
+    public function updateVideo(Request $request, $id)
+    {
+        $videos_input = $request->input();
+        $video = Video::where('id', $id);
+        $video->update($videos_input);
+        return response()->json(['description' => 'video update'], 200);
+    }
+
+    public function deleteVideos()
+    {
+        return response()->json([Video::truncate(), 'description' => 'Video delete'], 200);
+    }
+
+    public function deleteVideo($id)
+    {
+        $video = Video::where('id', $id);
+        $video->delete();
+        return response()->json(['description' => 'Video delete'], 200);
+    }
+
+    public function createVideo(Request $request)
+    {
+        $video = new Video;
+        $videos_input = $request->input();
+        $video->create($videos_input);
+        return response()->json(['description' => 'video created'], 200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HeroController;
+use App\Http\Controllers\VideoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,37 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['prefix' => 'show'], function(){
+
+    Route::get('/videos', [VideoController::class, 'showVideos']);
+    Route::get('/video/{id}', [VideoController::class, 'showVideo']);
+    Route::get('/heroes', [HeroController::class, 'showHeroes']);
+    Route::get('/hero/{id}', [HeroController::class, 'showHero']);
+
+});
+
+Route::group(['prefix' => 'update'], function(){
+
+    Route::patch('/video/{id}', [VideoController::class, 'updateVideo']);
+    Route::patch('/hero/{id}', [HeroController::class, 'updateHero']);
+
+});
+
+Route::group(['prefix' => 'delete'], function(){
+
+    Route::delete('/allvideos', [VideoController::class, 'deleteVideos']);
+    Route::delete('/allheroes', [HeroController::class, 'deleteHeroes']);
+    Route::delete('/video/{id}', [VideoController::class, 'deleteVideo']);
+    Route::delete('/hero/{hero}', [HeroController::class, 'deleteHero']);
+    
+
+});
+
+Route::group(['prefix' => 'create'], function(){
+
+    Route::post('/video', [VideoController::class, 'createVideo']);
+    Route::post('/hero', [HeroController::class, 'createHero']);
+
 });
