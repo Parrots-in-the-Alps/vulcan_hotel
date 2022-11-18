4986bb439c8f21b2a9537cf6268f393c87cf41c3
theocop73 <theo.colombel@le-campus-numerique.fr>
2022-11-18 14:43:43 +0100

    routes mailinglist

--- app/Http/Controllers/MailingListController.php
+++ app/Http/Controllers/MailingListController.php
@@ -2,9 +2,49 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
+use App\Models\MailingList;
 use Illuminate\Http\Request;
 
 class MailingListController extends Controller
 {
-    //
+    public function showMailingLists() { 
+        return response()->json(['mailinglist' => MailingList::all(), 'description' => 'OK'], 200);
+    }
+
+
+    public function showMailingList($id){
+         return response()->json(['mailinglist' => MailingList::find($id), 'description' => 'OK'], 200);
+    }
+
+    public function createMailingList(Request $request)
+{
+    $input = $request->input();
+    $mailingList = MailingList::create(
+        $input
+    );
+    return response()->json(['message' => 'MailingList created successfully!'], 200);
+}
+
+public function updateMailingList(Request $request, $mailingList)
+{
+    $input = $request->input();
+        $mailingList = MailingList::where('id', $mailingList)->update(
+            $input
+        );
+    return response()->json(['message' => 'MailingList updated successfully!'], 200);
+}
+
+
+public function deleteMailingList(MailingList $mailingList)
+{
+    $mailingList->delete();
+    return response()->json();
+}
+
+public function deleteMailingLists()
+{
+    MailingList::truncate();
+    return response()->json();
+}
 }
