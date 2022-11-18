dbb480658e1e7c593e160594d64183a2f021ba6e
theocop73 <theo.colombel@le-campus-numerique.fr>
2022-11-18 15:00:13 +0100

    fix room model

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -10,12 +10,6 @@ class Room extends Model
     use HasFactory;
 
 
-/**
-     * The primary key associated with the table.
-     *
-     * @var string
-     */
-    protected $primaryKey = 'room_id';
 
     protected $fillable = ['name', 'number', 'type', 'capacity', 'price', 'status', 'image', 'description'];
 }
