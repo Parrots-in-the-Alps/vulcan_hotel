f0e79ee2c5b5298ced475b72b848e66c7f6f66af
PierreOudot <114479562+PierreOudot@users.noreply.github.com>
2022-11-18 13:51:53 +0100

    gitignore updated with resources/js/App.js

---
+++ resources/js/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <h1 v-for="element in response" :key="element.id">{{element.slogan}}</h1>
+    <h1 class="text-7xl font-bold underline">
+cucul
+</h1>
+</template>
+
+<script>
+
+export default {
+  name: "App.vue",
+  data() {
+          return {
+              response: []
+          }
+      },
+      async mounted() {
+          const response = await axios.get('api/show/videos');
+          console.log(response.data)
+          this.response = response.data['videos'];
+      }
+}
+</script>
+
+
+<style scoped>
+    h1{
+        color:red
+    }
+</style>
