b30f427813e9bc320d7fe46ca356a0f916af8ad5
theocop73 <theo.colombel@le-campus-numerique.fr>
2022-11-24 16:59:52 +0100

    actuality + header

--- resources/js/components/Actualities.vue
+++ resources/js/components/Actualities.vue
@@ -1,7 +1,25 @@
 
 <template>
 
+<div class="flex flex-row mt-16 justify-center items-center">
+    <div v-for="element in response" :key="element.id" class="max-w-sm mr-5 max-h-sm rounded overflow-hidden shadow-lg">
+        <img class="object-cover h-48 w-96" :src="'/images/actualities/' + element.image" alt="Sunset in the mountains">
+        <div class="px-6 py-4">
+            <div class="font-bold text-xl mb-2 font-Cinzel">{{ element.title }}
+            </div>
+                <p class="text-gray-700 text-base font-Philosopher">
+                    {{ element.description }} 
+                </p>
+        </div>
 
+    </div>
+</div> 
+
+
+
+
+
+  
 
 </template>
 
@@ -17,8 +35,9 @@ export default {
     async mounted() {
         const response = await axios.get('api/show/actualities');
         console.log(response.data)
-        this.response = response.data['actuality'];
-    }
+        this.response = response.data['data'];
+    },
+
 }
 </script>
 
