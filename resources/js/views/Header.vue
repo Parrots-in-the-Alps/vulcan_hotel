d38a5da120ccb3de500dce4cd5b2ce7963fdb7d2
theocop73 <theo.colombel@le-campus-numerique.fr>
2022-11-25 10:40:27 +0100

    fix header

--- resources/js/views/Header.vue
+++ resources/js/views/Header.vue
@@ -33,18 +33,7 @@
   </div>
 </div>
 </nav>
-     <div class="flex flex-row bg-">
-        <div>
-            toto
-        </div>
-        <div>
-            tata
-        </div>
-        <div>
-            tutu
-        </div>
-
-    </div>
+   
    
    
 </template>
