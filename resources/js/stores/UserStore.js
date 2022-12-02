c994dc28291417b3b86a6814a5b66d2f578aa387
pierre <114479562+PierreOudot@users.noreply.github.com>
2022-12-02 14:59:23 +0100

    stores

---
+++ resources/js/stores/UserStore.js
@@ -0,0 +1,19 @@
+import { defineStore } from 'pinia'
+import axios from "axios"
+
+
+export const useUserStore = defineStore('user',{
+    state: () =>({
+        
+    }),
+    
+    getters:{
+
+    },
+
+    actions: {
+
+        
+    },
+    
+})
