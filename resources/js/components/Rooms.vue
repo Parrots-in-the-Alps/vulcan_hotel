017d1596dafd74a0ed1c1f5b7e945b575a656fcc
theocop73 <theo.colombel@le-campus-numerique.fr>
2022-12-02 17:03:06 +0100

    component room

--- resources/js/components/Rooms.vue
+++ resources/js/components/Rooms.vue
@@ -14,7 +14,7 @@
   </div> -->
 
 
-<div class="h-screen flex justify-center items-center">
+<div class=" flex justify-center items-center">
 
    <div v-for="room in response" :key="room.id" class="p-1">
             <div class="group relative block overflow-hidden rounded-md transition-all duration-500">
@@ -22,7 +22,7 @@
                                 <img class="object-cover h-48 w-96" :src="'/images/rooms/' + room.image">
                             </a>
                             <div class="absolute -bottom-52 group-hover:bottom-2 right-2 left-2 transition-all duration-500 bg-backGround dark:bg-slate-900 p-4 rounded shadow dark:shadow-gray-700">
-                                <a href="#" class="hover:text-primary-600 text-lg transition duration-500 font-medium">{{room.name}}</a>
+                                <a href="#" class="hover:text-primary-600 text-lg transition duration-500 font-medium text-white">{{room.name}}</a>
                                 <h6 class="text-slate-400">{{room.description}}</h6>
                             </div>
                         </div>
