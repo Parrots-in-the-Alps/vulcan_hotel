02e8f08c6eeb56dbd687abe199567ac2bc7e938b
PierreOudot <114479562+PierreOudot@users.noreply.github.com>
2022-12-05 16:24:28 +0100

    reviews implementing base

--- resources/js/components/Reviews.vue
+++ resources/js/components/Reviews.vue
@@ -1,12 +1,69 @@
 <template>
+    <div class="">
+        <div class="card w-96 bg-backGround shadow-xl">
+            <div class="flex flex-row justify-between">
+                <div class="avatar">
+                    <div class="w-24 mask mask-triangle overflow-visible">
+                        <img src="https://placeimg.com/192/192/people" />
+                    </div>
+                </div>
+                <div>
+                    <h1 class="font-Cinzel text-accent">Toto</h1>
+                </div>
+                <div class="flex flex-row self-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-star" width="24"
+                        height="24" viewBox="0 0 24 24" stroke-width="1" stroke="#ff5c04" fill="#ff5c04"
+                        stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                        <path
+                            d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z">
+                        </path>
+                    </svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-star" width="24"
+                        height="24" viewBox="0 0 24 24" stroke-width="1" stroke="#ff5c04" fill="#ff5c04"
+                        stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                        <path
+                            d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z">
+                        </path>
+                    </svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-star" width="24"
+                        height="24" viewBox="0 0 24 24" stroke-width="1" stroke="#ff5c04" fill="#ff5c04"
+                        stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                        <path
+                            d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z">
+                        </path>
+                    </svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-star" width="24"
+                        height="24" viewBox="0 0 24 24" stroke-width="1" stroke="#ff5c04" fill="#ff5c04"
+                        stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                        <path
+                            d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z">
+                        </path>
+                    </svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-star" width="24"
+                        height="24" viewBox="0 0 24 24" stroke-width="1" stroke="#ff5c04" fill="#243236"
+                        stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                        <path
+                            d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z">
+                        </path>
+                    </svg>
+                </div>
+            </div>
 
+
+        </div>
+    </div>
 
 </template>
     
 <script>
 
 export default {
-    name: "reviews.vue",inject:[
+    name: "reviews.vue", inject: [
         'isFrench',
         'reviews'
     ],
@@ -20,4 +77,5 @@ export default {
     
     
 <style scoped>
+
 </style>
