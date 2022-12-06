16a9a43bdb231bcf13e4f9960565a2586b899ac8
theocop73 <theo.colombel@le-campus-numerique.fr>
2022-12-06 11:29:29 +0100

    update contentSeparator

--- resources/js/components/ContentSeparator.vue
+++ resources/js/components/ContentSeparator.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="relative flex py-5 items-center">
     <div class="flex-grow border-t border-persimmon"></div>
-    <span class="font-Cinzel flex-shrink mx-4 text-persimmon">{{menuTitle}}</span>
+    <span v-if="menuTitle != null" class="font-Cinzel flex-shrink mx-4 text-persimmon">{{menuTitle}}</span>
     <div class="flex-grow border-t border-persimmon"></div>
 </div>
   </template>
