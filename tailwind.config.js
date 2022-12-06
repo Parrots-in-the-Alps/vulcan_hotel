5b52dd09f5c47065c5ed1a7ee6bd6fdda58b57e9
pierre <114479562+PierreOudot@users.noreply.github.com>
2022-12-06 15:17:08 +0100

    user reviews done

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,6 +27,16 @@ module.exports = {
   },
   theme: {
     extend: {
+      borderWidth: {
+        DEFAULT: '1px',
+        '0': '0',
+        '1.5': '1.5px',
+        '2': '2px',
+        '3': '3px',
+        '4': '4px',
+        '6': '6px',
+        '8': '8px',
+      },
       colors: {
         'backGround': '#2f2f2f',
         'callToAction': '#FF5C04',
