16db9cce1cbf95f461a1f255bc27bb6208e6794b
pierre <114479562+PierreOudot@users.noreply.github.com>
2022-12-07 14:02:28 +0100

    resa

--- resources/js/components/resa/ResaStays.vue
+++ resources/js/components/resa/ResaStays.vue
@@ -1,15 +1,14 @@
 <template>
     <div class="w-full h-full md:w-3/4  lg:w-2/3 xl:w-1/2 2xl:w-1/2 rounded-2xl shadow-xl flex flex-col items-center">
         <div class="self-center mt-6 mb-12">
-            <h1 class="text-secondary font-Cinzel ">SERVICES</h1>
+            <h1 class="text-secondary font-Cinzel ">stays</h1>
         </div>
-        <div v-for="service in serviceStore.activeServices" :key="service.index" class="w-1/2 h-2/3 flex flex-row justify-between ">
-                <div>
-                    <input type="checkbox" checked="checked" class="checkbox checkbox-secondary cursor-pointer" />
-                </div>
-                <div class="label-text text-CadetBlue font-Philosopher">{{ service.title }} </div>
-                <div class="label-text text-CadetBlue font-Philosopher">{{ service.price }} €$</div>
+        <div class="w-1/2 h-2/3 flex flex-row justify-between ">
+                
            
+        </div>
+        <div>
+
         </div>
         <div class="flex flex-row justify-between gap-x-12 mt-12 mb-6">
             <button v-if="isFrench" class="btn btn-secondary font-Cinzel w-28 text-base-100">Précédent</button>
@@ -23,10 +22,10 @@
     
 <script>
 import { mapStores } from 'pinia';
-import { useServiceStore } from '../stores/ServiceStore';
+import { useRoomStore } from '../../stores/ServiceStore';
 
 export default {
-    name: "ResaServices",
+    name: "ResaStays",
     inject: [
         'isFrench',
     ],
@@ -36,7 +35,7 @@ export default {
         }
     },
     computed: {
-        ...mapStores(useServiceStore)
+        ...mapStores(useRoomStore)
     }
 }
 </script>
