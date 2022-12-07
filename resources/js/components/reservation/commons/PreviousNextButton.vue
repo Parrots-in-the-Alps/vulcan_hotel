652b74c1914efb26e35c3ccbd342f35b66735102
pierre <114479562+PierreOudot@users.noreply.github.com>
2022-12-07 16:11:44 +0100

    prevnextbutt

--- resources/js/components/reservation/commons/PreviousNextButton.vue
+++ resources/js/components/reservation/commons/PreviousNextButton.vue
@@ -1,10 +1,16 @@
 <template>
 
     <div class="flex flex-row justify-between gap-x-12 mt-12 mb-6">
-        <button v-if="isFrench" class="btn btn-secondary font-Cinzel w-28 text-base-100">Précédent</button>
-        <button v-else class="btn btn-secondary font-Cinzel w-28 text-base-100">previous</button>
-        <button v-if="isFrench" class="btn btn-secondary w-28 font-Cinzel text-base-100">suivant</button>
-        <button v-else class="btn btn-secondary font-Cinzel w-28 text-base-100">next</button>
+        <router-link :to="previousRoute">
+            <button v-if="isFrench" class="btn btn-secondary font-Cinzel w-28 text-base-100">Précédent</button>
+            <button v-else class="btn btn-secondary font-Cinzel w-28 text-base-100">previous</button>
+        </router-link>
+        <router-link :to="nextRoute">
+            <button v-if="isFrench" class="btn btn-secondary w-28 font-Cinzel text-base-100">suivant</button>
+            <button v-else class="btn btn-secondary font-Cinzel w-28 text-base-100">next</button>
+        </router-link>
+
+
     </div>
 
 </template>
@@ -16,6 +22,10 @@ export default {
     inject: [
         'isFrench',
     ],
+    props: {
+        nextRoute: String,
+        previousRoute: String
+    }
 }
 </script>
 
