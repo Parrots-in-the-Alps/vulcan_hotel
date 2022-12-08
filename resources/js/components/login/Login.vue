ebb79187ed7573f5b92368c8befbec624bad8525
theocop73 <theo.colombel@le-campus-numerique.fr>
2022-12-08 09:53:45 +0100

    ajout vue register/login

---
+++ resources/js/components/login/Login.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="grid grid-cols-4 gap-4">
+        <div class="col-span-4 card-title self-center font-['Cinzel']">Login</div>
+       
+        <div>
+           
+            
+    
+            <div class="flex items-center">
+                <input type="text" placeholder="Email" class="input input-bordered w-full max-w-xs" />
+            
+            </div>
+        </div>
+    
+
+    
+        <div>
+           
+    
+            <div class="flex items-center">
+                <input type="text" placeholder="Password" class="input input-bordered w-full max-w-xs" />
+            
+            </div>
+    
+      
+        </div>
+    
+    </div>
+    
+    
+        
+      
+        
+        
+     
+    
+    </template>
+    
+    <script>
+    
+    
+    export default {
+        name: "Register",
+        inject: [
+            'isFrench',
+        ],
+    }
+    </script>
+    
+    
+    <style scoped>
+    
+    </style>
