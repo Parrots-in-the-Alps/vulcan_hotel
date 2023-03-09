a9e9f33334a0da395374b32f978b32ffc53678e3
theocop73 <theo.colombel@le-campus-numerique.fr>
2023-03-09 13:51:12 +0100

    APPLI SAUVE

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -16,78 +16,78 @@
 
 import Header from './components/commons/Header.vue';
 import Footer from './components/commons/Footer.vue';
-// import { useServiceStore } from './stores/ServiceStore.js';
-// import {useRoomStore} from './stores/RoomStore.js';
-// import { mapStores } from 'pinia';
+import { useServiceStore } from './stores/ServiceStore.js';
+import {useRoomStore} from './stores/RoomStore.js';
+import { mapStores } from 'pinia';
 import { computed } from 'vue';
 
 export default {
     name: "App.vue",
     components: { Header, Footer },
-    // beforeMount() {
-        // this.serviceStore.fetchActiveServices();
-        // this.roomStore.fetchActiveRooms();
-        // this.fetchActualities();
-        // this.fetchHero();
-        // this.fetchFooter();
-        // this.fetchReviews();
-        // this.fetchVideos();
-    // },
+    beforeMount() {
+        this.serviceStore.fetchActiveServices();
+        this.roomStore.fetchActiveRooms();
+        this.fetchActualities();
+        this.fetchHero();
+        this.fetchFooter();
+        this.fetchReviews();
+        this.fetchVideos();
+    },
     data() {
         return {
             isFrench: (navigator.language.startsWith("fr")),
-            // actualities: [],
-            // hero: [],
-            // footer: [],
-            // reviews: [],
-            // videos: []
+            actualities: [],
+            hero: [],
+            footer: [],
+            reviews: [],
+            videos: []
         }
     },
     provide() {
         return {
             isFrench: computed(() => this.isFrench),
-            // actualities: computed(() => this.actualities),
-            // hero: computed(() => this.hero),
-            // footer: computed(() => this.footer),
-            // reviews: computed(() => this.reviews),
-            // videos: computed(() => this.videos)
+            actualities: computed(() => this.actualities),
+            hero: computed(() => this.hero),
+            footer: computed(() => this.footer),
+            reviews: computed(() => this.reviews),
+            videos: computed(() => this.videos)
         }
     },
-    // computed: {
-    //     ...mapStores(useServiceStore, useRoomStore)
-    // },
-    // methods: {
+    computed: {
+        ...mapStores(useServiceStore, useRoomStore)
+    },
+    methods: {
 
-    //     async fetchActualities() {
-    //         const response = await axios.get('api/actualities/active');
-    //         console.log(response.data)
-    //         this.actualities = response.data['data'];
-    //     },
+        async fetchActualities() {
+            const response = await axios.get('api/actualities/active');
+            console.log(response.data)
+            this.actualities = response.data['data'];
+        },
 
-    //     async fetchHero() {
-    //         const response = await axios.get('api/heroes/active');
-    //         console.log(response.data)
-    //         this.hero = response.data['data'];
-    //     },
+        async fetchHero() {
+            const response = await axios.get('api/heroes/active');
+            console.log(response.data)
+            this.hero = response.data['data'];
+        },
 
-    //     async fetchFooter(){
-    //         const response = await axios.get('api/footers/active');
-    //         console.log(response.data)
-    //         this.footer = response.data['data'];
-    //     },
+        async fetchFooter(){
+            const response = await axios.get('api/footers/active');
+            console.log(response.data)
+            this.footer = response.data['data'];
+        },
 
-    //     async fetchReviews(){
-    //         const response = await axios.get('api/reviews/active');
-    //         console.log(response.data)
-    //         this.reviews = response.data['data'];
-    //     },
+        async fetchReviews(){
+            const response = await axios.get('api/reviews/active');
+            console.log(response.data)
+            this.reviews = response.data['data'];
+        },
 
-    //     async fetchVideos(){
-    //         const response = await axios.get('api/videos/active');
-    //         console.log(response.data)
-    //         this.videos = response.data['data'];
-    //     }
-    // }
+        async fetchVideos(){
+            const response = await axios.get('api/videos/active');
+            console.log(response.data)
+            this.videos = response.data['data'];
+        }
+    }
 }
 </script>
 
