ba8b01b4f2fc8a257e2232b7fb9e3294cf8f6cbf
theocop73 <theo.colombel@le-campus-numerique.fr>
2023-03-09 16:22:36 +0100

    bookbutton is smart now :)

--- resources/js/components/commons/BookButton.vue
+++ resources/js/components/commons/BookButton.vue
@@ -1,14 +1,34 @@
 
 <template>
+     <div  v-if="this.logged == true" class="self-center">
+        <router-link to="/reservation/stays">
     <button class="btn btn-secondary font-Cinzel text-base-100">{{isFrench ? "Réserver" : "Book Now" }}</button>
+        </router-link>
+</div>
+
+<div  v-else class="self-center">
+        <router-link to="/auth/login">
+    <button v-on:click="greet" class="btn btn-secondary font-Cinzel text-base-100">{{isFrench ? "Réserver" : "Book Now" }}</button>
+        </router-link>
+</div>
 </template>
   
 <script>
+import { useUserStore } from '../../stores/UserStore';
+import { mapActions, mapState } from 'pinia';
 export default {
     name: "bookButton",
     inject:[
         'isFrench'
     ],
+    computed: {
+        ...mapState(useUserStore, ['user', 'logged'])
+    },
+    methods: {
+        greet: function () {
+            window.alert("Vous devez être connecté pour effectuer une réservation");
+    },
+    },
 }
 </script>
 
