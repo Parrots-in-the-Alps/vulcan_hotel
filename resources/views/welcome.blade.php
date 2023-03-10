4e4cce6e7c40ede5e0a99cf5c9671369f00ce014
theocop73 <theo.colombel@le-campus-numerique.fr>
2023-03-10 12:29:01 +0100

    favicon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,8 +5,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link rel="shortcut icon" href="{{ asset('css/favicon.ico') }}">
 
-        <title>test</title>
+        <title>Vulcan</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
