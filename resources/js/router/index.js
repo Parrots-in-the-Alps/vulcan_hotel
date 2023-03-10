a5b6f2240090c54fcdd0d97dc72139b8a630a0fd
theocop73 <theo.colombel@le-campus-numerique.fr>
2023-03-10 09:47:39 +0100

    add scrollbehavior to router

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -47,7 +47,17 @@ const router = createRouter({
                 },
             ]
         },
-    ]
+    ],
+    // scrollBehavior(to, from, savedPosition) {
+        
+    //     if (to.hash) {
+    //         return {
+    //             el: to.hash,
+    //             behavior: 'smooth',
+    //         }
+    //     }
+    //     // return {x: 0, y: 0};
+    // },
 });
 
 export default router
