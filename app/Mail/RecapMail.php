a85c6c1f624b0045c2bb256b2230ea1e33592fa3
theocop73 <theo.colombel@le-campus-numerique.fr>
2023-03-10 15:46:39 +0100

    cron working i SWEAR

---
+++ app/Mail/RecapMail.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use App\Models\Reservation;
+
+class RecapMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $reservation;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(Reservation $reservation)
+    {
+        $this->reservation = $reservation;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('email.recapmail')
+                    ->subject('séjour dans 1 semaine')
+                    ->with([
+                        'reservation' => $this->reservation
+        ]);
+    }
+}
---
+++ resources/views/email/recapmail.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>ItsolutionStuff.com</title>
+</head>
+<body>
+    <h1>{{ $reservation->user->name }}</h1>
+
+   
+    <p>Thank you</p>
+</body>
+</html>
