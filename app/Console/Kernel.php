355df55da33ead42744ee8397925bcda7159dd88
NickelChr0me <matthias.codvelle@le-campus-numerique.fr>
2023-03-13 13:32:56 +0100

    add qr_code - code wifi

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('emails:send')->daily();
+        $schedule->command('emails:send')->everyMinute();
     }
 
     /**
--- resources/views/email/recapmail.blade.php
+++ resources/views/email/recapmail.blade.php
@@ -8,7 +8,8 @@
 
     <p>{{$temperature->current_weather->temperature}}</p>
 
-   
+    <img src="https://api.qrserver.com/v1/create-qr-code/?data=code%20Wifi:%20betsyMougnagna!&size=100x100" alt="qr code" />
+
     <p>Thank you</p>
 </body>
 </html>
