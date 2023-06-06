228097af2082dbe20f6105fe32fd714d8d20c663
NickelChr0me <matthias.codvelle@le-campus-numerique.fr>
2023-06-06 14:13:07 +0200

    update CORS header in axios

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -11,6 +11,7 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*',
 window.axios.defaults.withCredentials = true;
 
 /**
