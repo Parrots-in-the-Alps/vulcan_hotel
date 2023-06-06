a71c801898497126e2f2dc253e79543023141be5
NickelChr0me <matthias.codvelle@le-campus-numerique.fr>
2023-06-06 14:45:22 +0200

    add Cors middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,5 +65,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'setLocale' => \App\Http\Middleware\SetLocale::class,
+        'cors' => \App\Http\Middleware\Cors::class,
     ];
 }
